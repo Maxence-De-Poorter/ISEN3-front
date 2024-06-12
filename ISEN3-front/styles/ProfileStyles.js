ae488b0aa2239d4c774655985de0bd3329c8d926
Maxence DE POORTER <github.flo1u@passmail.net>
2024-06-12 12:41:35 +0200

    Refactoring/Styling Profile Page

---
+++ ISEN3-front/styles/ProfileStyles.js
@@ -0,0 +1,55 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        height: "100%",
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 20,
+        paddingHorizontal: 20,
+    },
+    profileContainer: {
+        flex: 1,
+        alignItems: 'center',
+        paddingHorizontal: 20,
+    },
+    closeButton: {
+        marginTop: 50,
+        marginLeft: 10,
+    },
+    infoContainer: {
+        padding: 20,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        margin: 20,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 5,
+        elevation: 5,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 10,
+        paddingLeft: 10,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: '#FFFFFF',
+        fontSize: 16,
+        marginLeft: 10,
+    },
+});
+
+export default styles;
