c41e6634b543f3dc0bdf49207fba68469958275e
Maxence DE POORTER <github.flo1u@passmail.net>
2024-06-12 13:38:52 +0200

    Update HomeScreen.js

--- ISEN3-front/screens/HomeScreen.js
+++ ISEN3-front/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext} from 'react';
 import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
 import {AuthContext} from "../context/AuthContext";
 
