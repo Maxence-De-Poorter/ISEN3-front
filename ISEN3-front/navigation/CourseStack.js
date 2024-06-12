d8839ec7c3cfe0734bb43f357e5a199e93fe5302
Maxence DE POORTER <github.flo1u@passmail.net>
2024-06-12 16:06:43 +0200

    Renaming

--- ISEN3-front/navigation/CourseStack.js
+++ ISEN3-front/navigation/CourseStack.js
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { View } from 'react-native';
 import CoursesScreen from '../screens/CoursesScreen';
-import EnrollmentHistory from '../components/EnrollmentHistory';
 import ProfileButton from '../components/ProfileButton';
 import Tickets from '../components/Tickets';
 import { AuthContext } from '../context/AuthContext';
@@ -10,7 +9,7 @@ import { AuthContext } from '../context/AuthContext';
 const Stack = createStackNavigator();
 const headerBackgroundColor = 'white';
 
-function CalendarStack({ navigation }) {
+function CourseStack({ navigation }) {
     const { isLoggedIn, user } = useContext(AuthContext);
     const ticket = user ? user.ticket : 0;
 
@@ -32,13 +31,8 @@ function CalendarStack({ navigation }) {
                     ),
                 }}
             />
-            <Stack.Screen
-                name="EnrollmentHistory"
-                component={EnrollmentHistory}
-                options={{ headerTitle: 'Enrollment History' }}
-            />
         </Stack.Navigator>
     );
 }
 
-export default CalendarStack;
+export default CourseStack;
